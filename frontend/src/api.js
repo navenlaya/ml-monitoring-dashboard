6fb584323ca5fa54bbf38c44b20d9083ac144c60
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-07-14 10:16:25 -0700

    Fetches predictions CSV Log from backend

---
+++ frontend/src/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+// Send prediction request
+export const sendPrediction = (data) => API.post('/predict', data);
+
+// Fetch predictions CSV log
+export const getPredictions = async () => {
+  const res = await fetch('http://127.0.0.1:8000/logs/predictions.csv');
+  return await res.text();
+};
